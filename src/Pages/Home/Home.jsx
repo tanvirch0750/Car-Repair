707e4cd356acfa7353e968846db2066bcd017648
tanvirch0750 <tanvir0750@yahoo.com>
2022-04-15 01:34:41 +0600

    implemented carousel functionality

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import Carousel, {
+  CarouselItem,
+} from "../../Components/CarouselItem/CarouselItem";
 import "./Home.css";
 
 const Home = () => {
   return (
     <div>
-      <h1>This is home page</h1>
+      <Carousel>
+        <CarouselItem>
+          <h2>item 1 </h2>
+        </CarouselItem>
+        <CarouselItem>
+          <h2>item 2</h2>
+        </CarouselItem>
+        <CarouselItem>
+          <h3>item 3</h3>
+        </CarouselItem>
+      </Carousel>
     </div>
   );
 };
