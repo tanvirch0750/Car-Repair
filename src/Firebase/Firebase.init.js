64fd252948111d145ca1d6d48444a7da45ec5a57
tanvirch0750 <tanvir0750@yahoo.com>
2022-04-15 17:16:39 +0600

    install firebase

---
+++ src/Firebase/Firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA-FNavJ1FDR5eg941jfjd6T2xW6H1Pmz4",
+  authDomain: "car-repair-21fbc.firebaseapp.com",
+  projectId: "car-repair-21fbc",
+  storageBucket: "car-repair-21fbc.appspot.com",
+  messagingSenderId: "660832771070",
+  appId: "1:660832771070:web:2931880855987211e63eaf",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
