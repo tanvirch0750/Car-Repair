fec62e0ce30a57d800ccbffac40c76e0012fb5a5
tanvirch0750 <tanvir0750@yahoo.com>
2022-04-16 15:28:15 +0600

    implemented featured on functionality

---
+++ src/Components/FeaturedOn/FeaturedOn.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import featuredImgfour from "../../images/featured-on/cbs.webp";
+import featuredImgTwo from "../../images/featured-on/forbes.webp";
+import featuredImgFive from "../../images/featured-on/inc.webp";
+import featuredImgOne from "../../images/featured-on/nbc.webp";
+import featuredImgThree from "../../images/featured-on/newyorktimes.webp";
+import "./FeaturedOn.css";
+
+const featuredImages = [
+  featuredImgOne,
+  featuredImgTwo,
+  featuredImgThree,
+  featuredImgfour,
+  featuredImgFive,
+];
+
+const FeaturedOn = () => {
+  return (
+    <div className="featured-on container">
+      <h2>Featured On</h2>
+      {featuredImages.map((img, idx) => (
+        <img src={img} key={idx} alt="featured on"></img>
+      ))}
+    </div>
+  );
+};
+
+export default FeaturedOn;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Carousel, {
   CarouselItem,
 } from "../../Components/CarouselItem/CarouselItem";
+import FeaturedOn from "../../Components/FeaturedOn/FeaturedOn";
 import bannerImageOne from "../../images/banner-image-1.jpg";
 import bannerImageTwo from "../../images/banner-image-2.jpg";
 import bannerImageThree from "../../images/banner-image-3.jpg";
@@ -27,56 +28,32 @@ const carouselItems = [
 
 const Home = () => {
   return (
-    <div>
-      <Carousel>
-        {carouselItems.map((item) => (
-          <CarouselItem key={item.id} item={item}>
-            <div className="caro-inner">
-              <div>
-                <h1>
-                  {item.heading.split(" ").slice(0, 2).join(" ")} <br></br>{" "}
-                  {item.heading.split(" ").slice(2).join(" ")}
-                </h1>
-                <p>
-                  We believe in providing top quality workmanship and are so{" "}
-                  <br></br> confident in our level of service that we back it up
-                  with a <br></br> good quality. Your safety is our first
-                  priority.
-                </p>
-                <button className="btn btn-lg">Our Services</button>
+    <>
+      <div>
+        <Carousel>
+          {carouselItems.map((item) => (
+            <CarouselItem key={item.id} item={item}>
+              <div className="caro-inner">
+                <div>
+                  <h1>
+                    {item.heading.split(" ").slice(0, 2).join(" ")} <br></br>{" "}
+                    {item.heading.split(" ").slice(2).join(" ")}
+                  </h1>
+                  <p>
+                    We believe in providing top quality workmanship and are so{" "}
+                    <br></br> confident in our level of service that we back it
+                    up with a <br></br> good quality. Your safety is our first
+                    priority.
+                  </p>
+                  <button className="btn btn-lg">Our Services</button>
+                </div>
               </div>
-            </div>
-          </CarouselItem>
-        ))}
-        {/* <CarouselItem>
-          <div className="caro-item-1">
-            <div className="caro-inner">
-              <h1>
-                Tuneup Your Car <br></br> to next level
-              </h1>
-            </div>
-          </div>
-        </CarouselItem>
-        <CarouselItem>
-          <div className="caro-item-2">
-            <div className="caro-inner">
-              <h1>
-                Highly skilled <br></br> certified enginniers
-              </h1>
-            </div>
-          </div>
-        </CarouselItem>
-        <CarouselItem>
-          <div className="caro-item-3">
-            <div className="carro-inner">
-              <h1>
-                Modern Workshop <br></br> for car repairs
-              </h1>
-            </div>
-          </div>
-        </CarouselItem> */}
-      </Carousel>
-    </div>
+            </CarouselItem>
+          ))}
+        </Carousel>
+      </div>
+      <FeaturedOn />
+    </>
   );
 };
 
